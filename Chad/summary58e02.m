7386b2dc2c212fd8abb8c57140fe001c20cbcb69
moonjeli <161738387+moonjeli@users.noreply.github.com>
2024-07-11 13:59:55 -0400

    more nsf grant figure updates

--- Chad/summary58e02.m
+++ Chad/summary58e02.m
@@ -28,7 +28,7 @@ folder = '06282024/fly1/session2';
 data.fly06282024_001.session2 = extract_data(folder);
 data.fly06282024_001.session2.fr = 30/1;
 
-%% 
+%% Start of TH 58E02 flies
 
 folder = '07022024/fly1/session1';
 
@@ -47,11 +47,27 @@ folder = '07082024/fly1/session1';
 
 data.fly07082024_000.session1 = extract_data(folder);
 data.fly07082024_000.session1.fr = 30/1;
+
+%% too much motion
+
+% folder = '07102024/fly2/session1';
+% 
+% data.fly07102024_002.session1 = extract_data(folder);
+% data.fly07102024_002.session1.fr = 30/1;
+
+%% 
+
+folder = '07102024/fly2/session2';
+
+data.fly07102024_002.session2 = extract_data(folder);
+data.fly07102024_002.session2.fr = 30/1;
 %%
 lobe_ids = {"gamma5", "gamma4", "gamma3", "gamma2", "beta"};
 
 analysis_58e02(data, "movspd", lobe_ids)
+analysis_58e02(data, "inthead", lobe_ids)
 analysis_58e02(data, "drotvlx", lobe_ids)
+analysis_58e02(data, "net_move", lobe_ids)
 
 analysis_58e02(data, "drotvly", lobe_ids)
 analysis_58e02(data, "drotvlz", lobe_ids)
